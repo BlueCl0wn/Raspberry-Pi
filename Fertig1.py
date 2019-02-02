e8e90fe661cd5081be683dc81c4819ea80071f18
BlueCl0wn <45670653+BlueCl0wn@users.noreply.github.com>
2019-02-02 20:16:15 +0100

    Add files via upload

---
+++ Fertig1.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(23,GPIO.IN)
+
+while True:
+    if GPIO.input(23) == GPIO.HIGH:
+        GPIO.output(24, GPIO.LOW)
+        time.sleep(2)
+        print("on")
+    else:
+        GPIO.output(24, GPIO.HIGH)
+        time.sleep(2)
+        print ("off")
